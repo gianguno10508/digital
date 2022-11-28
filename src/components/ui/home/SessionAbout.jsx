a6de989393a1503214a839c434094c66256158fd
GiangNV <giangnv@arrowhitech.co>
2022-11-28 11:04:55 +0700

    ertr

--- src/components/ui/home/SessionAbout.jsx
+++ src/components/ui/home/SessionAbout.jsx
@@ -1,1 +1,14 @@
 // round, title, description, subtitle
+const SessionAbout = ({...rest}) => {
+    <div className="sub-heading">
+        <span>{rest.sub-heading}</span>
+    </div>
+    {    rest.title && (
+        <h2>{rest.title}</h2>
+    )}
+    <div className="description">{rest.description}</div>
+    {rest.sub-content-second && (
+        <p className="sub-content-second">{rest.sub-content-second}</p>
+    )}
+}
+export default SessionAbout
