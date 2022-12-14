af6d1e5bac50939d405f604fae2fa45b9ba63e73
GiangNV <giangnv@arrowhitech.co>
2022-12-14 15:17:57 +0700

    fdgfasd

---
+++ src/asset/fakedata/oursolution/contentOursolution.jsx
@@ -0,0 +1,39 @@
+const ContentOurSolution = [
+    {
+        title: '<h3 className="title">Magento</h3>',
+        description: '<p>As one of key strategic partners of <strong>Magento – the World’s # 1 open-source eCommerce platform</strong>, AHT Solution takes pride in consulting and implementing<a href="#"> Magento projects</a> for businesses with the most professional and accurate workflow.</p><p>Whether you are just starting out with Magento, are migrating from another platform, or need ongoing support to optimize your existing website, we got your back.</p></div><h3 class="service-headding">Magento Services</h3><ul class="services row"><li class="item col-sm-12 col-md-6">Platform Migration</li><li class="item col-sm-12 col-md-6">Custom Development</li><li class="item col-sm-12 col-md-6">Audit &amp; Optimization</li><li class="item col-sm-12 col-md-6">Maintenance</li></ul>',
+        image:{
+            sourceUrl: 'https://onextdigital.com/wp-content/uploads/2022/07/magento.png'
+        },
+        location: "Left",
+        columnLeft: 5,
+    },
+    {
+        title: '<h3 className="title">Shopify</h3>',
+        description: '<p>As one of key strategic partners of <strong>Magento – the World’s # 1 open-source eCommerce platform</strong>, AHT Solution takes pride in consulting and implementing<a href="#"> Magento projects</a> for businesses with the most professional and accurate workflow.</p><p>Whether you are just starting out with Magento, are migrating from another platform, or need ongoing support to optimize your existing website, we got your back.</p></div><h3 class="service-headding">Magento Services</h3><ul class="services row"><li class="item col-sm-12 col-md-6">Platform Migration</li><li class="item col-sm-12 col-md-6">Custom Development</li><li class="item col-sm-12 col-md-6">Audit &amp; Optimization</li><li class="item col-sm-12 col-md-6">Maintenance</li></ul>',
+        image:{
+            sourceUrl: 'https://onextdigital.com/wp-content/uploads/2022/07/shopify.png'
+        },
+        location: "Left",
+        columnLeft: 5,
+    },
+    {
+        title: '<h3 className="title">Wordpress</h3>',
+        description: '<p>As one of key strategic partners of <strong>Magento – the World’s # 1 open-source eCommerce platform</strong>, AHT Solution takes pride in consulting and implementing<a href="#"> Magento projects</a> for businesses with the most professional and accurate workflow.</p><p>Whether you are just starting out with Magento, are migrating from another platform, or need ongoing support to optimize your existing website, we got your back.</p></div><h3 class="service-headding">Magento Services</h3><ul class="services row"><li class="item col-sm-12 col-md-6">Platform Migration</li><li class="item col-sm-12 col-md-6">Custom Development</li><li class="item col-sm-12 col-md-6">Audit &amp; Optimization</li><li class="item col-sm-12 col-md-6">Maintenance</li></ul>',
+        image:{
+            sourceUrl: 'https://onextdigital.com/wp-content/uploads/2022/07/wordpress.png'
+        },
+        location: "Left",
+        columnLeft: 5,
+    },
+    {
+        title: '<h3 className="title">Headless solution</h3>',
+        description: '<p>Headless is <strong>the best-suited approach</strong> for solving website speed, upgrade,and microservices issues due to rapid front-end development and speed optimization.</p><p>Being a pioneer of headless technology, ONextDigital leverages the power of headless architecture to build experience-driven solutions for your business. With our vast knowledge and experience in the software industry we’re confident in implementing end-to-end development of headless solutions for your businesses.</p>',
+        image:{
+            sourceUrl: 'https://onextdigital.com/wp-content/uploads/2022/07/headless.png'
+        },
+        location: "Left",
+        columnLeft: 5,
+    }
+]
+export default ContentOurSolution;
