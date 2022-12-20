3f5545140a287b09cd4f34e4758001d3b8c6a21b
GiangNV <giangnv@arrowhitech.co>
2022-12-20 14:53:55 +0700

    ggfghg

--- src/components/common/ScrollEffect.jsx
+++ src/components/common/ScrollEffect.jsx
@@ -1,9 +1,16 @@
 import ScrollAnimation from "react-animate-on-scroll";
 const ScrollEffect = ({children, durationNumber}) => {
     return(
-        <ScrollAnimation duration={durationNumber} animateIn="fadeInUp">
-            {children}
-        </ScrollAnimation>
+        durationNumber ? 
+            <ScrollAnimation duration={durationNumber} animateIn="fadeInUp">
+                {children}
+            </ScrollAnimation>
+            :
+            <ScrollAnimation duration={1.5} animateIn="fadeInUp">
+                {children}
+            </ScrollAnimation>
+        
+
     )
 }
 export default ScrollEffect;
